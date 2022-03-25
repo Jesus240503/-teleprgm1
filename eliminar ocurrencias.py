719b2c1a2ee2ad202f0e17892f744c8c5e9947cd
Jesus240503 <100143723+Jesus240503@users.noreply.github.com>
2022-03-25 15:13:19 -0400

    Add files via upload

---
+++ eliminar ocurrencias.py
@@ -0,0 +1,12 @@
+vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86 , 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98 , 73, 57]
+
+for vEdad in vEdades:
+    if vEdad == 10:
+        vEdades.remove(10)
+
+for vEdad in vEdades:
+    print("edad:", vEdad,"años")
+
+Total = len(vEdades)
+print("Total:", Total + 1)
+print(vEdades)
